275ce20be532d259ce9afddc573bc88ce570649e
GeorgiaLR <cordonnier.l@gmail.com>
2020-04-28 13:58:49 +0200

    security update

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,3 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAv9cb5ZR2XqxO8HYEEpmLsqtr-hHKR_Uc",
-    authDomain: "watchlist-65f91.firebaseapp.com",
-    databaseURL: "https://watchlist-65f91.firebaseio.com",
-    projectId: "watchlist-65f91",
-    storageBucket: "watchlist-65f91.appspot.com",
-    messagingSenderId: "287236196773"
+    
 }
